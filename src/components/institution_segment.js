9221765fb5716d5e1199312ced93b4533f97d1fe
wenxin-c <chenwenxin0825@gmail.com>
2023-06-06 10:11:38 +0800

    Add pictures for institution page

--- src/components/institution_segment.js
+++ src/components/institution_segment.js
@@ -7,109 +7,8 @@ import { Button, Popover } from 'antd';
 import NUS_logo from '../images/nus_logo.jpg'
 import NJC_logo from '../images/njc_logo.png'
 import CGS_logo from '../images/cgs_logo.png'
-
-const { Header, Content, Sider } = Layout;
-
-// function InstitutionSegment(){
-//     const {
-//         token: { colorBgContainer },
-//       } = theme.useToken();
-//     return (
-//         <div>
-//             <Content
-//             style={{
-//               padding: 24,
-//               margin: 0,
-//               minHeight: 280,
-//               background: colorBgContainer,
-//             }}
-//           >
-
-//             {/* The part of the code is to update education
-//                 Having one more Card element to insert in one more education*/}
-//             <div className="site-card-border-less-wrapper" >
-//                 <Card
-//                 title=<div className='schoolTitle'><img src={NUS_logo} width='50px'/><span className='schoolName'>National University of Singapore</span></div>
-//                 bordered={false}
-//                 style={{
-//                     width: 340,
-//                     marginRight:'10px',
-//                     marginBottom:'10px',
-//                 }}
-//                 >
-//                 <p>Status: Undergraduate</p>
-//                 <p>Major: Computer Engineering</p>
-               
-//                 </Card>
-
-//                 <Card
-//                 title=<div className='schoolTitle'><img src={NJC_logo} width='50px'/><span className='schoolName'>National Junior College</span></div>
-//                 bordered={false}
-//                 style={{
-//                     width: 330,
-//                     marginRight:'10px',
-//                     marginBottom:'10px',
-//                 }}
-//                 >
-//                 <p>Degree: High School</p>
-//                 <p>Stream: Science Stream</p>
-//                 <p>Course: 
-//                     <ul>
-//                         <li>Mathematics</li>
-//                         <li>Physics</li>
-//                         <li>Chemistry</li>
-//                         <li>Economics</li>
-//                         <li>General Paper</li>
-//                         <li>H3 Physics</li>
-//                     </ul>
-//                 </p>
-//                 <p>A Level: 
-//                     <ul>
-//                         <li>Rank points: 87.5</li>
-//                         <li>H3 Physics: Merit</li>
-//                     </ul>
-//                 </p>
-//                 </Card>
-
-//                 <Card
-//                 title=<div className='schoolTitle'><img src={CGS_logo} width='40px' height='40px'/><span className='schoolName'>Crescent Girls' School</span></div>
-//                 bordered={false}
-//                 style={{
-//                     width: 330,
-//                     marginRight:'10px',
-//                     marginBottom:'10px',
-//                 }}
-//                 >
-//                 <p>Degree: Secondary School</p>
-//                 <p>O Level:
-//                     <ul>
-//                         <li>L1R5: 6</li>
-//                     </ul>
-//                 </p>
-//                 </Card>
-
-//                 <Card
-//                 title="Additional Information"
-//                 bordered={false}
-//                 style={{
-//                     width: 250,
-//                     marginRight:'10px',
-//                     marginBottom:'10px',
-//                 }}
-//                 >
-//                 <p>Experience: I was having education in China before I went to Sinagpore in 2016. </p>
-
-//                 </Card>
-
-//             </div>
-            
-//           </Content>
-
-//         </div>
-//     );
-
-// }
-
+import NUS_pic from "../images/nus.png"
+import NJC_pic from "../images/njc.png"
 
 const textUni = <span>National University of Singapore</span>;
 const contentUni = (
@@ -172,7 +71,7 @@ const buttonWidth = 90;
 const InstitutionSegment = () => (
   <div className = "education-segment">
     <h1 className = "education-title">Institutions</h1>
-    {/* <div className = "education-content"> */}
+    <div className = "education-content">
         <div className = "education-button"
         style={{
             width: buttonWidth * 4,
@@ -198,8 +97,11 @@ const InstitutionSegment = () => (
             </Popover>
 
         </div>
-        <div className = "education-picture"></div>
-    {/* </div> */}
+        <div className = "education-picture">
+            <img className = "nus-pic" src = {NUS_pic} alt = "NUS picture"/>
+            <img className = "njc-pic" src = {NJC_pic} alt = "NJC picture"/>
+        </div>
+    </div>
     
 
   </div>
